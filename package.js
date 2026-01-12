0b36386be0ab516645fe43a23ffd73cf951f7a0a
manbang2868-droid <manbang2868@gmail.com>
2026-01-12 12:15:07 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "giaycung-api",
+  "private": true,
+  "type": "module",
+  "engines": { "node": ">=18" },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.19.2"
+  }
+}
